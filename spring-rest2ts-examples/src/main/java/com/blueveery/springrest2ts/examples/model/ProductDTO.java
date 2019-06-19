f2d14f9543e9d61c6293c99cb659554735034f8a
Konrad Stachera <konrad.stachera@blue-veery.com>
2019-06-19 15:20:37 +0200

    JsonIgnoreProperties test

--- spring-rest2ts-examples/src/main/java/com/blueveery/springrest2ts/examples/model/ProductDTO.java
+++ spring-rest2ts-examples/src/main/java/com/blueveery/springrest2ts/examples/model/ProductDTO.java
@@ -1,14 +1,16 @@
 package com.blueveery.springrest2ts.examples.model;
 
 import com.blueveery.springrest2ts.examples.model.core.BaseDTO;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import java.math.BigInteger;
 import java.time.LocalDateTime;
 
-
+@JsonIgnoreProperties(value = {"barcode"}, allowGetters = true)
 public class ProductDTO extends BaseDTO {
     public String name;
     public BigInteger price;
+    public String barcode;
     public LocalDateTime expirationDate;
     public ManufacturerDTO manufacturer;
 }
