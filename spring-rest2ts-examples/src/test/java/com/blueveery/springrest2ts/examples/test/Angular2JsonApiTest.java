110f18ce81ef487b19fe171b01a3c8f383eae821
Tomasz Wozniak <tomasz.wozniak@blue-veery.com>
2020-01-11 12:17:34 +0100

    new modules for angular2-jsonapi converter

--- spring-rest2ts-examples/src/test/java/com/blueveery/springrest2ts/examples/test/Angular2JsonApiTest.java
+++ spring-rest2ts-examples/src/test/java/com/blueveery/springrest2ts/examples/test/Angular2JsonApiTest.java
@@ -4,10 +4,8 @@ package com.blueveery.springrest2ts.examples.test;
 import com.blueveery.springrest2ts.Rest2tsGenerator;
 import com.blueveery.springrest2ts.converters.JacksonObjectMapper;
 import com.blueveery.springrest2ts.converters.ModelClassesToTsAngular2JsonApiConverter;
-import com.blueveery.springrest2ts.converters.SpringDataRestConversionExtension;
 import com.blueveery.springrest2ts.examples.model.core.ParametrizedBaseDTO;
 import com.blueveery.springrest2ts.filters.ExtendsJavaTypeFilter;
-import com.blueveery.springrest2ts.naming.SubstringClassNameMapper;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import org.junit.Test;
 
