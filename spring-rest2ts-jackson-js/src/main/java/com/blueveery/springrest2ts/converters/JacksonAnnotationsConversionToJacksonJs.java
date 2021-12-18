598b705caa9cf54e50101a99ddc77791b14d90c0
Tomasz Wozniak <tomaszwozniak@interia.pl>
2021-12-19 00:08:56 +0100

    JsonTypeInfo#property conversion

--- spring-rest2ts-jackson-js/src/main/java/com/blueveery/springrest2ts/converters/JacksonAnnotationsConversionToJacksonJs.java
+++ spring-rest2ts-jackson-js/src/main/java/com/blueveery/springrest2ts/converters/JacksonAnnotationsConversionToJacksonJs.java
@@ -9,15 +9,21 @@ import com.blueveery.springrest2ts.tsmodel.TSLiteral;
 import com.blueveery.springrest2ts.tsmodel.TSScopedElement;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
+import java.util.Arrays;
+
 public class JacksonAnnotationsConversionToJacksonJs extends TypeBasedConversionToJacksonJs {
 
     protected final TSFunction jsonTypeInfoFunction;
     protected final TSEnum jsonTypeInfoIdEnum;
+    protected final TSEnum JsonTypeInfoAs;
 
     public JacksonAnnotationsConversionToJacksonJs() {
         jsonTypeInfoFunction = new TSFunction("JsonTypeInfo", jacksonJSModule);
+
         jsonTypeInfoIdEnum = new TSEnum("JsonTypeInfoId", jacksonJSModule);
         jsonTypeInfoIdEnum.add("NAME");
+
+        JsonTypeInfoAs = new TSEnum("JsonTypeInfoAs", jacksonJSModule);
     }
 
     @Override
@@ -27,17 +33,64 @@ public class JacksonAnnotationsConversionToJacksonJs extends TypeBasedConversion
 
         JsonTypeInfo jsonTypeInfo = (JsonTypeInfo) javaType.getAnnotation(JsonTypeInfo.class);
         if (jsonTypeInfo != null) {
-            addJacksonAnnotation(tsClass, jsonTypeInfo);
+            addJsonTypeInfoAnnotation(tsClass, jsonTypeInfo);
         }
     }
 
-    private void addJacksonAnnotation(TSClass tsClass, JsonTypeInfo jsonTypeInfo) {
+    private void addJsonTypeInfoAnnotation(TSClass tsClass, JsonTypeInfo jsonTypeInfo) {
         TSDecorator jsonTypeInfoDecorator = new TSDecorator(jsonTypeInfoFunction);
         TSJsonLiteral jsonTypeInfoLiteral = new TSJsonLiteral();
         jsonTypeInfoDecorator.getTsLiteralList().add(jsonTypeInfoLiteral);
         tsClass.addScopedTypeUsage(jsonTypeInfoFunction);
-        jsonTypeInfoLiteral.getFieldMap().put("use", new TSLiteral("", TypeMapper.tsAny, "JsonTypeInfoId.NAME"));
-        tsClass.addScopedTypeUsage(jsonTypeInfoIdEnum);
+
+        convertJsonTypeInfoUse(tsClass, jsonTypeInfo, jsonTypeInfoLiteral);
+        if (convertJsonTypeInfoInclude(tsClass, jsonTypeInfo, jsonTypeInfoLiteral)) {
+            convertJsonTypeInfoProperty(jsonTypeInfo, jsonTypeInfoLiteral);
+        }
+
         tsClass.getTsDecoratorList().add(jsonTypeInfoDecorator);
+    }
+
+    private void convertJsonTypeInfoProperty(JsonTypeInfo jsonTypeInfo, TSJsonLiteral jsonTypeInfoLiteral) {
+        String propertyName = jsonTypeInfo.property();
+        if("".equals(propertyName)) {
+            propertyName = jsonTypeInfo.use().getDefaultPropertyName();
+        }
+        if (!"@type".equals(propertyName)) {
+            jsonTypeInfoLiteral.getFieldMap().put("property", new TSLiteral("", TypeMapper.tsString, propertyName));
+        }
+    }
+
+    private void convertJsonTypeInfoUse(TSClass tsClass, JsonTypeInfo jsonTypeInfo, TSJsonLiteral jsonTypeInfoLiteral) {
+        if (Arrays.asList(JsonTypeInfo.Id.NAME, JsonTypeInfo.Id.CLASS, JsonTypeInfo.Id.MINIMAL_CLASS).contains(jsonTypeInfo.use())) {
+            jsonTypeInfoLiteral.getFieldMap().put("use", new TSLiteral("", TypeMapper.tsAny, "JsonTypeInfoId.NAME"));
+            tsClass.addScopedTypeUsage(jsonTypeInfoIdEnum);
+        } else {
+            throw new IllegalStateException("JsonTypeInfo.use()" + jsonTypeInfo.use() + " is not supported in jackson-js");
+        }
+    }
+
+    private boolean convertJsonTypeInfoInclude(TSClass tsClass, JsonTypeInfo jsonTypeInfo, TSJsonLiteral jsonTypeInfoLiteral) {
+        boolean isProperty = false;
+        String tsInclude = null;
+        switch(jsonTypeInfo.include()) {
+            case PROPERTY:
+            case EXISTING_PROPERTY:
+                tsInclude = "JsonTypeInfoAs.PROPERTY";
+                isProperty = true;
+                break;
+            case WRAPPER_OBJECT:
+                tsInclude = "JsonTypeInfoAs.WRAPPER_OBJECT";
+                break;
+            case WRAPPER_ARRAY:
+                tsInclude = "JsonTypeInfoAs.WRAPPER_ARRAY";
+                break;
+            case EXTERNAL_PROPERTY:
+                throw new IllegalStateException("EXTERNAL_PROPERTY is not supported in jackson-js");
+
+        }
+        jsonTypeInfoLiteral.getFieldMap().put("include", new TSLiteral("", TypeMapper.tsAny, tsInclude));
+        tsClass.addScopedTypeUsage(JsonTypeInfoAs);
+        return isProperty;
     }
 }
--- spring-rest2ts-jackson-js/src/test/java/com/blueveery/springrest2ts/converters/JacksonAnnotationsConversionToJacksonJsTest.java
+++ spring-rest2ts-jackson-js/src/test/java/com/blueveery/springrest2ts/converters/JacksonAnnotationsConversionToJacksonJsTest.java
@@ -4,7 +4,11 @@ import com.blueveery.springrest2ts.filters.JavaTypeSetFilter;
 import com.blueveery.springrest2ts.tsmodel.TSClass;
 import com.blueveery.springrest2ts.tsmodel.TSDecorator;
 import com.blueveery.springrest2ts.tsmodel.TSImport;
+import com.blueveery.springrest2ts.tsmodel.TSJsonLiteral;
+import com.blueveery.springrest2ts.tsmodel.TSLiteral;
 import com.blueveery.springrest2ts.tsmodel.TSModule;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import org.assertj.core.util.Sets;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -29,21 +33,121 @@ public class JacksonAnnotationsConversionToJacksonJsTest extends JacksonJsTest {
     }
 
     @Test
-    public void jsonTypeInfoDecoratorShouldBeAddedBasedOnJsonTypeInfo() throws IOException {
-        SortedSet<TSModule> tsModules = tsGenerator.convert(javaPackageSet);
-        TSClass vehicle = (TSClass) findTSComplexElement(tsModules, Vehicle.class.getSimpleName());
-        Optional<TSDecorator> jsonClassType = findDecorator(jacksonAnnotationsConversion.jsonTypeInfoFunction, vehicle.getTsDecoratorList());
-        assertThat(jsonClassType).isPresent();
-        printTSElement(tsModules.first());
-    }
-
-    @Test
     public void jsonTypeInfoDecoratorShouldBeImportedWhenAdded() throws IOException {
         SortedSet<TSModule> tsModules = tsGenerator.convert(javaPackageSet);
         TSImport tsImport = tsModules.first().getImportMap().get(jacksonAnnotationsConversion.jacksonJSModule);
         assertThat(tsImport).isNotNull();
         assertThat(tsImport.getWhat()).contains(jacksonAnnotationsConversion.jsonTypeInfoFunction);
         assertThat(tsImport.getWhat()).contains(jacksonAnnotationsConversion.jsonTypeInfoIdEnum);
+        printTSElement(tsModules.first());
+    }
+
+    @Test
+    public void jsonTypeInfoDecoratorShouldBeAddedBasedOnJsonTypeInfo() throws IOException {
+        SortedSet<TSModule> tsModules = tsGenerator.convert(javaPackageSet);
+        TSClass vehicle = (TSClass) findTSComplexElement(tsModules, Vehicle.class.getSimpleName());
+        Optional<TSDecorator> jsonClassType = findDecorator(jacksonAnnotationsConversion.jsonTypeInfoFunction, vehicle.getTsDecoratorList());
+        assertThat(jsonClassType).isPresent();
+        TSJsonLiteral jsonLiteral = (TSJsonLiteral) jsonClassType.get().getTsLiteralList().stream().findFirst().get();
+        assertThat(jsonLiteral.getFieldMap().get("use")).isEqualTo(new TSLiteral("", TypeMapper.tsAny, "JsonTypeInfoId.NAME"));
+        printTSElement(tsModules.first());
+    }
+
+    @Test
+    public void jsonTypeInfoDecoratorShouldHaveIncludeSetToProperty() throws IOException {
+        @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY)
+        class Animal {}
+        tsGenerator.setModelClassesCondition(new JavaTypeSetFilter(Animal.class));
+        SortedSet<TSModule> tsModules = tsGenerator.convert(Sets.set(getClass().getPackage().getName()));
+        TSClass animal = (TSClass) findTSComplexElement(tsModules, Animal.class.getSimpleName());
+        Optional<TSDecorator> jsonClassType = findDecorator(jacksonAnnotationsConversion.jsonTypeInfoFunction, animal.getTsDecoratorList());
+        assertThat(jsonClassType).isPresent();
+        TSJsonLiteral jsonLiteral = (TSJsonLiteral) jsonClassType.get().getTsLiteralList().stream().findFirst().get();
+        assertThat(jsonLiteral.getFieldMap().get("include")).isEqualTo(new TSLiteral("", TypeMapper.tsAny, "JsonTypeInfoAs.PROPERTY"));
+        printTSElement(tsModules.first());
+    }
+
+    @Test
+    public void jsonTypeInfoDecoratorShouldHaveIncludeSetToWRAPPER_OBJECT() throws IOException {
+        @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.WRAPPER_OBJECT)
+        class Animal {}
+        tsGenerator.setModelClassesCondition(new JavaTypeSetFilter(Animal.class));
+        SortedSet<TSModule> tsModules = tsGenerator.convert(Sets.set(getClass().getPackage().getName()));
+        TSClass animal = (TSClass) findTSComplexElement(tsModules, Animal.class.getSimpleName());
+        Optional<TSDecorator> jsonClassType = findDecorator(jacksonAnnotationsConversion.jsonTypeInfoFunction, animal.getTsDecoratorList());
+        assertThat(jsonClassType).isPresent();
+        TSJsonLiteral jsonLiteral = (TSJsonLiteral) jsonClassType.get().getTsLiteralList().stream().findFirst().get();
+        assertThat(jsonLiteral.getFieldMap().get("include")).isEqualTo(new TSLiteral("", TypeMapper.tsAny, "JsonTypeInfoAs.WRAPPER_OBJECT"));
+        printTSElement(tsModules.first());
+    }
+
+    @Test
+    public void jsonTypeInfoDecoratorShouldHaveIncludeSetToWRAPPER_ARRAY() throws IOException {
+        @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.WRAPPER_ARRAY)
+        class Animal {}
+        tsGenerator.setModelClassesCondition(new JavaTypeSetFilter(Animal.class));
+        SortedSet<TSModule> tsModules = tsGenerator.convert(Sets.set(getClass().getPackage().getName()));
+        TSClass animal = (TSClass) findTSComplexElement(tsModules, Animal.class.getSimpleName());
+        Optional<TSDecorator> jsonClassType = findDecorator(jacksonAnnotationsConversion.jsonTypeInfoFunction, animal.getTsDecoratorList());
+        assertThat(jsonClassType).isPresent();
+        TSJsonLiteral jsonLiteral = (TSJsonLiteral) jsonClassType.get().getTsLiteralList().stream().findFirst().get();
+        assertThat(jsonLiteral.getFieldMap().get("include")).isEqualTo(new TSLiteral("", TypeMapper.tsAny, "JsonTypeInfoAs.WRAPPER_ARRAY"));
+        printTSElement(tsModules.first());
+    }
+
+    @Test
+    public void jsonTypeInfoDecoratorShouldSkipPropertyIfDefaultIsUsed() throws IOException {
+        @JsonTypeInfo(use = JsonTypeInfo.Id.NAME)
+        class Animal {}
+        tsGenerator.setModelClassesCondition(new JavaTypeSetFilter(Animal.class));
+        SortedSet<TSModule> tsModules = tsGenerator.convert(Sets.set(getClass().getPackage().getName()));
+        TSClass animal = (TSClass) findTSComplexElement(tsModules, Animal.class.getSimpleName());
+        Optional<TSDecorator> jsonClassType = findDecorator(jacksonAnnotationsConversion.jsonTypeInfoFunction, animal.getTsDecoratorList());
+        assertThat(jsonClassType).isPresent();
+        TSJsonLiteral jsonLiteral = (TSJsonLiteral) jsonClassType.get().getTsLiteralList().stream().findFirst().get();
+        assertThat(jsonLiteral.getFieldMap().get("property")).isNull();
+        printTSElement(tsModules.first());
+    }
+
+    @Test
+    public void jsonTypeInfoDecoratorShouldSetCorrectPropertyIfClassIsUsed() throws IOException {
+        @JsonTypeInfo(use = JsonTypeInfo.Id.CLASS)
+        class Animal {}
+        tsGenerator.setModelClassesCondition(new JavaTypeSetFilter(Animal.class));
+        SortedSet<TSModule> tsModules = tsGenerator.convert(Sets.set(getClass().getPackage().getName()));
+        TSClass animal = (TSClass) findTSComplexElement(tsModules, Animal.class.getSimpleName());
+        Optional<TSDecorator> jsonClassType = findDecorator(jacksonAnnotationsConversion.jsonTypeInfoFunction, animal.getTsDecoratorList());
+        assertThat(jsonClassType).isPresent();
+        TSJsonLiteral jsonLiteral = (TSJsonLiteral) jsonClassType.get().getTsLiteralList().stream().findFirst().get();
+        assertThat(jsonLiteral.getFieldMap().get("property")).isEqualTo(new TSLiteral("", TypeMapper.tsString, "@class"));
+        printTSElement(tsModules.first());
+    }
+
+    @Test
+    public void jsonTypeInfoDecoratorShouldSetCorrectPropertyIfMinimalClassIsUsed() throws IOException {
+        @JsonTypeInfo(use = JsonTypeInfo.Id.MINIMAL_CLASS)
+        class Animal {}
+        tsGenerator.setModelClassesCondition(new JavaTypeSetFilter(Animal.class));
+        SortedSet<TSModule> tsModules = tsGenerator.convert(Sets.set(getClass().getPackage().getName()));
+        TSClass animal = (TSClass) findTSComplexElement(tsModules, Animal.class.getSimpleName());
+        Optional<TSDecorator> jsonClassType = findDecorator(jacksonAnnotationsConversion.jsonTypeInfoFunction, animal.getTsDecoratorList());
+        assertThat(jsonClassType).isPresent();
+        TSJsonLiteral jsonLiteral = (TSJsonLiteral) jsonClassType.get().getTsLiteralList().stream().findFirst().get();
+        assertThat(jsonLiteral.getFieldMap().get("property")).isEqualTo(new TSLiteral("", TypeMapper.tsString, "@c"));
+        printTSElement(tsModules.first());
+    }
+
+    @Test
+    public void jsonTypeInfoDecoratorShouldSetCorrectPropertyIfCustomNameIsUsed() throws IOException {
+        @JsonTypeInfo(use = JsonTypeInfo.Id.MINIMAL_CLASS, property = "typeId")
+        class Animal {}
+        tsGenerator.setModelClassesCondition(new JavaTypeSetFilter(Animal.class));
+        SortedSet<TSModule> tsModules = tsGenerator.convert(Sets.set(getClass().getPackage().getName()));
+        TSClass animal = (TSClass) findTSComplexElement(tsModules, Animal.class.getSimpleName());
+        Optional<TSDecorator> jsonClassType = findDecorator(jacksonAnnotationsConversion.jsonTypeInfoFunction, animal.getTsDecoratorList());
+        assertThat(jsonClassType).isPresent();
+        TSJsonLiteral jsonLiteral = (TSJsonLiteral) jsonClassType.get().getTsLiteralList().stream().findFirst().get();
+        assertThat(jsonLiteral.getFieldMap().get("property")).isEqualTo(new TSLiteral("", TypeMapper.tsString, "typeId"));
         printTSElement(tsModules.first());
     }
 }
