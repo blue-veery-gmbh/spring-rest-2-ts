fa6cd95aa34e6da7efd170c81ebce04a20ccb718
Tomasz Wozniak <tomaszwozniak@interia.pl>
2022-08-19 11:28:31 +0200

    improved assertion in test to reproduce missing import for types used as parameters for generic type

--- spring-rest2ts-tests/src/main/java/com/blueveery/springrest2ts/tests/ComplexElementFinder.java
+++ spring-rest2ts-tests/src/main/java/com/blueveery/springrest2ts/tests/ComplexElementFinder.java
@@ -2,19 +2,20 @@ package com.blueveery.springrest2ts.tests;
 
 import com.blueveery.springrest2ts.tsmodel.TSComplexElement;
 import com.blueveery.springrest2ts.tsmodel.TSModule;
+import com.blueveery.springrest2ts.tsmodel.TSScopedElement;
 import java.util.SortedSet;
 
 public interface ComplexElementFinder {
   default TSComplexElement findTSComplexElement(SortedSet<TSModule> tsModules, String name) {
-    return (TSComplexElement) tsModules.first().
-                                       getScopedTypesSet()
-                                       .stream()
-                                       .filter(t -> name.equals(t.getName()))
-                                       .findFirst().get();
+    return (TSComplexElement) findScopedElement(tsModules.first(), name);
   }
 
   default TSComplexElement findTSComplexElement(TSModule tsModules, String name) {
-    return (TSComplexElement) tsModules
+    return (TSComplexElement) findScopedElement(tsModules, name);
+  }
+
+  default TSScopedElement findScopedElement(TSModule tsModules, String name) {
+    return tsModules
         .getScopedTypesSet()
         .stream()
         .filter(t -> name.equals(t.getName()))
---
+++ spring-rest2ts-tests/src/test/java/com/blueveery/springrest2ts/converters/enums/SingleResult.java
@@ -0,0 +1,4 @@
+package com.blueveery.springrest2ts.converters.enums;
+
+public class SingleResult<T> {
+}
